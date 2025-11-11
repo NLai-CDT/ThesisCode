99845306c8acf52cc22115783b0ec3d81726e006
NLai-CDT <92583747+NLai-CDT@users.noreply.github.com>
2025-11-11 16:15:54 +0000

    Add Cross-Subject Files

---
+++ CrossSubjectValidation/DatasetPreparation_CrossSubejctValidation.m
@@ -0,0 +1,46 @@
+%% Prepare dataset - temporal dynamics for combined subjects
+
+% Load dataset- EEGSignals
+
+% Set parameters
+seed=1;
+nbFilterPairs = 108*0.5;
+
+%% Training dataset: CSP filter, Projected Signals, Features 
+cd /home/nicole/Documents/AudioCueWalking_analysis/Scripts/
+
+CSP_Filter = [];
+TrainingRaw = [];
+TrainingProjected= [];
+TrainingFeatures= [];
+
+TrainingProjected.x = [];
+TrainingProjected.y = TrainingDataset.y;
+
+%CSP_Filter = learnCSPLagrangian(TrainingDataset);
+%RCSP_Filter = learn_DL_CSPLagrangian_auto(TrainingDataset);
+CSP_Riemann_Filter = learnCSPLagrangian_Riemann(TrainingDataset);
+
+TrainingRaw=extractRawSignals(TrainingDataset, nbFilterPairs);
+
+TrainingProjected.x=extractProjectedSignals(TrainingDataset, CSP_Riemann_Filter, nbFilterPairs);
+
+TrainingFeatures=extractCSPFeatures(TrainingDataset, CSP_Riemann_Filter, nbFilterPairs);
+
+
+
+% Testing dataset: Projected Signals, Features 
+
+TestingRaw=[];
+TestingProjected=[];
+TestingFeatures=[];
+
+TestingProjected.x = [];
+TestingProjected.y = TestingDataset.y;
+
+
+TestingRaw=extractRawSignals(TestingDataset, nbFilterPairs);
+
+TestingProjected.x=extractProjectedSignals(TestingDataset, CSP_Riemann_Filter, nbFilterPairs);
+
+TestingFeatures=extractCSPFeatures(TestingDataset, CSP_Riemann_Filter, nbFilterPairs);
---
+++ CrossSubjectValidation/SVMClassification_CrossSubject.m
@@ -0,0 +1,72 @@
+%% SVM classification: Kernel = Linear, C = 1 
+clear all 
+clc
+  
+% Set parameters
+Condition = 'Features'; % Raw or Features
+LoadingPath='/home/nicole/Documents/AudioCueWalking_analysis/Variables/AdaptVsNon/Combined_Subjects/TrainingTestingDatasets/SeperateConditions/Delay';
+seed=1;
+
+
+% Load dataset 
+if strcmp(Condition, 'Raw')
+    load(fullfile(LoadingPath, 'TrainingRaw.mat'));
+    load(fullfile(LoadingPath, 'TestingRaw.mat'));
+
+elseif strcmp(Condition, 'Features')
+    load(fullfile(LoadingPath, 'TrainingFeatures.mat'));
+    load(fullfile(LoadingPath, 'TestingFeatures.mat'));
+
+elseif strcmp(Condition, 'Features_R')
+    load(fullfile(LoadingPath, 'TrainingFeatures_R.mat'));
+    load(fullfile(LoadingPath, 'TestingFeatures_R.mat'));
+end
+
+
+% Create X_train, Y_train, X_test, and Y_test variables
+if strcmp(Condition, 'Raw')
+    X_train=TrainingRaw(:, 1:end-1);
+    Y_train=TrainingRaw(:, end);
+
+    X_test=TestingRaw(:, 1:end-1);
+    Y_test=TestingRaw(:, end);
+
+elseif strcmp(Condition, 'Features')
+    X_train=TrainingFeatures(:, 1:end-1);
+    Y_train=TrainingFeatures(:, end);
+
+    X_test=TestingFeatures(:, 1:end-1);
+    Y_test=TestingFeatures(:, end);
+
+elseif strcmp(Condition, 'Features_R')
+    X_train=TrainingFeatures_R(:, 1:end-1);
+    Y_train=TrainingFeatures_R(:, end);
+
+    X_test=TestingFeatures_R(:, 1:end-1);
+    Y_test=TestingFeatures_R(:, end);
+
+end
+
+% Train classifier
+rng(seed)
+SVMModel= fitcsvm(X_train, Y_train, 'KernelFunction','linear', 'BoxConstraint',1);
+
+% Test classifier
+
+predictions = predict(SVMModel, X_test);
+
+% Performance metrics
+
+confusionMatrix = confusionmat(Y_test, predictions);
+
+accuracy = sum(diag(confusionMatrix)) / sum(confusionMatrix(:));
+
+recall = confusionMatrix(1, 1) / sum(confusionMatrix(1, :));
+
+precision = confusionMatrix(1, 1) / sum(confusionMatrix(:, 1));
+
+F1_score = 2 * (precision * recall) / (precision + recall);
+
+
+performanceMetrics = [accuracy; recall; precision; F1_score];
+
